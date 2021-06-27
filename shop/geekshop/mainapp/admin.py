e365b4353691971e3ec58dc770c611857942af4b
pshenoki <suslik1991@yandex.ru>
2021-06-27 18:31:19 +0300

    lesson_3_Orlov_Vadim

--- shop/geekshop/mainapp/admin.py
+++ shop/geekshop/mainapp/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ProductCategory, Product, MenuCategory
 
-# Register your models here.
+admin.site.register(ProductCategory)
+admin.site.register(Product)
+admin.site.register(MenuCategory)
--- shop/geekshop/mainapp/views.py
+++ shop/geekshop/mainapp/views.py
@@ -1,19 +1,15 @@
 from django.shortcuts import render
 
+from mainapp.models import MenuCategory
+
 
 def products(request):
     title = "продукты/каталог"
-    links_menu = [
-        {'href': 'products_all', 'name': 'все'},
-        {'href': 'products_home', 'name': 'дом'},
-        {'href': 'products_office', 'name': 'офис'},
-        {'href': 'products_modern', 'name': 'модерн'},
-        {'href': 'products_classic', 'name': 'классика'},
-]
+    links_menu = MenuCategory.objects.all()
+
     context = {
         'title': title,
         'links_menu': links_menu,
-
     }
 
     return render(request=request, template_name='mainapp/products.html', context=context)

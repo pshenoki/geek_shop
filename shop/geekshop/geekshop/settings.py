5efd301781a7f18e2e64c7690b5cbdac51734fca
pshenoki <suslik1991@yandex.ru>
2021-08-19 23:27:42 +0300

    lesson_2_7_Orlov_Vadim

--- shop/geekshop/geekshop/settings.py
+++ shop/geekshop/geekshop/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -159,7 +159,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "geekshop", "static"),
     os.path.join(BASE_DIR, "basketapp", "static"),
@@ -208,6 +208,8 @@ AUTHENTICATION_BACKENDS = (
 # если сипользуем env
 SOCIAL_AUTH_VK_OAUTH2_KEY = env('SOCIAL_AUTH_VK_OAUTH2_ID')
 SOCIAL_AUTH_VK_OAUTH2_SECRET = env('SOCIAL_AUTH_VK_OAUTH2_KEY')
+
+LOW_CACHE = False
 
 if DEBUG:
    def show_toolbar(request):
